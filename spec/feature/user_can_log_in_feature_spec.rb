3e8ec4ad0cab69788cd78e5b9d352232dbf6e0f9
odeane_croney <odeane_croney@yahoo.com>
2020-06-26 13:26:16 +0200

    testing sad path seperately

--- spec/feature/user_can_log_in_feature_spec.rb
+++ spec/feature/user_can_log_in_feature_spec.rb
@@ -23,11 +23,27 @@ feature 'User can log in' do
           expect(page).to have_content 'Signed in successfully.'
     end
 
-    it 'If the email format is incorrect or the password is too short' do
+    it 'If the email format is incorrect' do
     fill_in 'Email', :with => '&5aygde'
-    fill_in 'Password', :with => '22'
+    fill_in 'Password', :with => '12345678'
     click_on 'Log in'
       expect(page).to have_content 'Invalid Email or password.'
+    end
+
+    
+    it 'If the password is incorrect' do
+      fill_in 'Email', :with => 'user@mail.com'
+      fill_in 'Password', :with => '2234'
+      click_on 'Log in'
+        expect(page).to have_content 'Invalid Email or password.'
+    end
+    
+    
+    it 'If the password is too short' do
+      fill_in 'Email', :with => '&5aygde'
+      fill_in 'Password', :with => '11'
+      click_on 'Log in'
+        expect(page).to have_content 'Invalid Email or password.'
     end
 end
 
