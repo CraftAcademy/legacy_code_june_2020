4e52cce5aa8327004d30d468bdb1c1bce2ceff70
Sima <sima.boreiri@gmail.com>
2020-06-27 18:31:51 +0200

    formates the spec codes

--- spec/features/navigation_bar_spec.rb
+++ spec/features/navigation_bar_spec.rb
@@ -2,16 +2,15 @@ require "rails_helper"
 feature ' user see content of navigation bar' do
   context 'navigation bar' do
     let(:user) { create(:user, name: 'sima', email: 'example@mail.com')}
-     before do 
+      before do 
         login_as(user, scope: :user)
         visit root_path
-     end
-     it 'display all navigation link ' do
+    end
+    it 'display all navigation link ' do
        expect(page).to have_content 'CA Mailboxer'
        expect(page).to have_content 'Hello, sima'
        expect(page).to have_content 'Inbox'
        expect(page).to have_content 'Logout'
     end
-
   end
 end
